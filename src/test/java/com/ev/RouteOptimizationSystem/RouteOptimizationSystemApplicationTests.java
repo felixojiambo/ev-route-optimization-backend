88abe2cec7f272e2014031788327f09561b5b2be
felixojiambo <felixojiamboe@gmail.com>
2024-08-30 17:07:40 +0300

    initial commit

---
+++ src/test/java/com/ev/RouteOptimizationSystem/RouteOptimizationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev.RouteOptimizationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteOptimizationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
